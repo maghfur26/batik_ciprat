0120ad93125bde77376ef24833168e6b24ba5aa9
maghfur26 <maghfurhasani9@gmail.com>
2024-04-28 20:02:55 +0700

    fiks pull

--- my-react-app/src/Component/Fragment/Kelebihan.jsx
+++ my-react-app/src/Component/Fragment/Kelebihan.jsx
@@ -2,19 +2,35 @@ import { ReactTyped } from "react-typed";
 
 export default function Kelebihan() {
   return (
-<<<<<<< HEAD
-    <div className="container-kelebihan min-w-full w-full bg-cover h-fit my-14 shadow-md shadow-slate-500 rounded-sm" style={{ backgroundImage: "url('./public/image/batik-bg.jpg')" }}>
-=======
-    <div className="container-kelebihan min-w-full w-full bg-cover h-fit my-40 shadow-md shadow-slate-500 rounded-sm" style={{ backgroundImage: "url('./public/image/batik-bg.jpg')" }}>
->>>>>>> cb937cb (add product)
-      <div className="uppercase text-slate-300 pt-10 pb-5 px-8 font-Bebas tracking-[2px] text-3xl md:text-5xl md:px-10 opacity-65">
-        <ReactTyped strings={["Batik ciprat", "Indonesia"]} typeSpeed={200} backSpeed={100} loop />
+    <>
+      <div
+        className="container-kelebihan min-w-full w-full bg-cover h-fit my-40 shadow-md shadow-slate-500 rounded-sm"
+        style={{ backgroundImage: "url('./public/image/batik-bg.jpg')" }}
+      >
+        <div className="uppercase text-slate-300 pt-10 pb-5 px-8 font-Bebas tracking-[2px] text-3xl md:text-5xl md:px-10 opacity-65">
+          <ReactTyped
+            strings={["Batik ciprat", "Indonesia"]}
+            typeSpeed={200}
+            backSpeed={100}
+            loop
+          />
+        </div>
+        <p className="text-white px-8 pb-20 md:max-w-screen-md md:mx-auto font-sans">
+          &quot; Batik ciprat adalah salah satu teknik pembuatan batik yang unik
+          dan kreatif, di mana proses pembuatannya melibatkan percikan atau
+          semprotan tinta secara bebas ke permukaan kain. Teknik ini
+          memungkinkan seniman untuk menciptakan pola dan motif yang tidak
+          terbatas, menciptakan batik dengan tampilan yang eksperimental dan
+          artistik.{" "}
+          <span className="hidden md:flex">
+            Dengan menggunakan alat semprot atau sikat, tinta batik diberikan ke
+            kain dengan berbagai intensitas, menciptakan efek visual yang
+            dinamis dan beragam. Batik ciprat sering kali memberikan kesan yang
+            spontan dan abstrak, memungkinkan para seniman untuk mengekspresikan
+            kreativitas mereka dengan kebebasan dan keunikan yang besar.&quot;
+          </span>
+        </p>
       </div>
-      <p className="text-white px-8 pb-20 md:max-w-screen-md md:mx-auto font-sans">
-        &quot; Batik ciprat adalah salah satu teknik pembuatan batik yang unik dan kreatif, di mana proses pembuatannya melibatkan percikan atau semprotan tinta secara bebas ke permukaan kain. Teknik ini memungkinkan seniman untuk menciptakan pola
-        dan motif yang tidak terbatas, menciptakan batik dengan tampilan yang eksperimental dan artistik. <span className="hidden md:flex">Dengan menggunakan alat semprot atau sikat, tinta batik diberikan ke kain dengan berbagai intensitas, menciptakan efek visual yang
-        dinamis dan beragam. Batik ciprat sering kali memberikan kesan yang spontan dan abstrak, memungkinkan para seniman untuk mengekspresikan kreativitas mereka dengan kebebasan dan keunikan yang besar.&quot;</span>
-      </p>
-    </div>
+    </>
   );
 }
--- my-react-app/src/Component/Fragment/Top_saller.jsx
+++ my-react-app/src/Component/Fragment/Top_saller.jsx
@@ -3,14 +3,6 @@ import AreaCard from "../Element/card";
 const Top_saller = () => {
   return (
     <>
-<<<<<<< HEAD
-      <h2 className="top_saller text-4xl text-center my-12 uppercase font-bold underline underline-offset-4">Top Saller</h2>
-      <div className="list_top_saller flex flex-wrap justify-center gap-3 md:gap-8">
-        <AreaCard image={"https://source.unsplash.com/random/?shoes"} />
-        <AreaCard image={"https://source.unsplash.com/random/?t-shirt"} />
-        <AreaCard image={"https://source.unsplash.com/random/?jacket"} />
-        <AreaCard image={"https://source.unsplash.com/random/?heat"} />
-=======
       <h2 className="top_saller text-orange-400 text-4xl text-center my-12 uppercase font-bold  underline-offset-4">
         Top Saller
       </h2>
@@ -47,10 +39,9 @@ const Top_saller = () => {
           <AreaCard image={"https://source.unsplash.com/random/?bag"} />
           <AreaCard image={"https://source.unsplash.com/random/?man-bag"} />
         </div>
->>>>>>> cb937cb (add product)
       </div>
     </>
-  );
-};
+  )
+}
 
 export default Top_saller;
